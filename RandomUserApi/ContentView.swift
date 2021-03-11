838999809d84829bfa1abd310727c45444e12eef
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-03-12 01:06:07 +0900

    introSpect 추가, 당겨서 새로고침 작업 완료

--- RandomUserApi/ContentView.swift
+++ RandomUserApi/ContentView.swift
@@ -6,20 +6,63 @@
 //
 
 import SwiftUI
+import UIKit
+import Introspect
+
+class RefreshControlHelper {
+    
+    //MARK: Properties
+    var parentContentView : ContentView?
+    var refreshControl : UIRefreshControl?
+    
+    @objc func didRefresh(){
+        print(#fileID, #function, #line, "")
+        guard let parentContentView = parentContentView,
+              let refreshControl = refreshControl else {
+            print("parentContentView, refreshControl 가 nil 입니다")
+            return
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+            print("리프레시가 되었다")
+            //MARK: - TODO : Api 다시 땡기기
+//            parentContentView.randomUserViewModel.fetchRandomUsers()
+            parentContentView.randomUserViewModel.refreshActionSubject.send()
+            refreshControl.endRefreshing()
+        })
+    }
+    
+}
 
 struct ContentView: View {
     
     @ObservedObject var randomUserViewModel = RandomUserViewModel()
+    
+    let refreshControlHelper = RefreshControlHelper()
     
     var body: some View {
         
         List(randomUserViewModel.randomUsers){ aRandomUser in
             RandomUserRowView(aRandomUser)
         }
-        
-//        List(0...100, id: \.self){ index in
-//            RandomUserRowView()
+        .introspectTableView{ self.configureRefreshControl($0) }
+//        .introspectTableView{ tableView in
+//            self.configureRefreshControl(tableView)
 //        }
+        
+    }
+    
+}
+
+//MARK: - Helper Methods
+extension ContentView {
+    fileprivate func configureRefreshControl(_ tableView: UITableView){
+        print(#fileID, #function, #line, "")
+        let myRefresh = UIRefreshControl()
+        myRefresh.tintColor = #colorLiteral(red: 1, green: 0.5433388929, blue: 0, alpha: 1)
+        refreshControlHelper.refreshControl = myRefresh
+        refreshControlHelper.parentContentView = self
+        myRefresh.addTarget(refreshControlHelper, action: #selector(RefreshControlHelper.didRefresh), for: .valueChanged)
+        tableView.refreshControl = myRefresh
     }
 }
 
